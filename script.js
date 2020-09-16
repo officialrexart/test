cec0832ef7e9ef25a08231a83a503feefd718f15
Officialrexart <mainrexart@gmail.com>
2020-09-16 16:05:20 +0100

    added files

---
+++ script.js
@@ -0,0 +1,17 @@
+
+    function check() {
+        let empty = document.getElementById("empty");
+        let email = document.getElementById("email-input").value;
+        if(email === "rex@gmail.com"){
+            empty.textContent="Welcome rex we have been expecting you, go to your console for the secreat code  ";
+        }
+        else if(email === ""){
+            empty.textContent="field cant be empty";
+            empty.style.marginLeft = "40%";
+        }
+        else{
+            empty.textContent="sorry we dont have any message for you thanks";
+            empty.style.marginLeft = "30%";
+              }
+          
+    }
